a6a4d5d3b7fa0f0cc67fd4c42c468a1e24e65602
momoka-A <100806831+momoka-A@users.noreply.github.com>
2022-04-20 23:25:52 +0900

    2-8
    foreach

--- index.php
+++ index.php
@@ -1,47 +1,45 @@
 <?php
-$countries = ['America', 'Japan', 'China', 'Korea'];
+// 配列
+$fruits = ['りんご', 'みかん', 'もも'];
 
-var_dump($countries);
+foreach($fruits as $value) {
+    echo $value;
+}
+
+echo '<br>';
 
 // 連想配列
-$fruits = ['apple' => 'りんご', 'orange' => 'みかん', 'grape' => 'ぶどう'];
+$fruits = ['apple' => 'りんご', 'orange' => 'みかん', 'peach' => 'もも'];
 
-echo $fruits['apple'];
-echo $fruits['orange'];
-echo $fruits['grape'];
-
-var_dump($fruits);
-
-$fruits = ['りんご', 'みかん', 'ぶどう'];
-$fruits[] = 'もも';
-var_dump($fruits);
+foreach($fruits as $key => $value){
+    echo $key;
+    echo $value;
+}
 
 echo '<br>';
 echo '<br>';
 echo '<br>';
-
-
-
-
 
 // 課題
-$color = ["red" => "赤", "blue" => "青", "green" => "緑"];
-var_dump($color);
+$fruits = ['apple' => 'りんご', 'orange' => 'みかん', 'peach' => 'もも'];
+
+foreach($fruits as $key => $value){
+    echo $key. 'といったら' . $value. '<br>';
+}
+
+
 echo '<br>';
-$color["yellow"] = "黄" ;
-var_dump($color);
 echo '<br>';
 echo '<br>';
 
-// ITエンジニアになって困らないための基本用語
-
-echo "プルリクエスト（マージリクエスト）" . "<br>";
-echo "開発者のローカルリポジトリでの変更を開発者に通知する機能";
+// IT用語
+echo "トランザクション" . "<br>";
+echo "ここからここまでワンセットな処理単位";
 echo '<br>';
 echo '<br>';
-echo "Git Flow" . "<br>";
-echo "masterやdevelopなど複数のブランチを用意し、効率的にチームで開発していく手法";
+echo "排他ロック" . "<br>";
+echo "データやファイルに対して複数のアクセスが見込まれる場合に同時アクセスにより不整合が発生することを防ぐため、あるトランザクションがデータやファイルにアクセスしている時は他トランザクションからはアクセスできないようにして直列に処理されるように制御すること";
 echo '<br>';
 echo '<br>';
-echo "CRON" . "<br>";
-echo "プログラムを定期的に自動実行させる仕組み";
+echo "チューニング" . "<br>";
+echo "いい感じになるように調整する作業";
