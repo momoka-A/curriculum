67db6bd4c27a02f2f9d3afb090976090e0e2fc08
momoka-A <100806831+momoka-A@users.noreply.github.com>
2022-05-16 23:08:14 +0900

    2-11
    フォームに入力した内容を取得する

---
+++ PHP/2-11/index.php
@@ -0,0 +1,26 @@
+<form action="result.php" method="post">
+    名前:<input type="text" name="my_name" />
+    <br>
+    メールアドレス:<input type="text" name="email" />
+    <br>
+    パスワード:<input type="password" name="password" />
+    <br>
+    <input type="submit" value="送信" />
+</form>
+
+<?php
+
+// IT用語
+echo "仕様書・設計書" . "<br>";
+echo "仕様書は何を作るをかを説明した資料、設計書はそれをどうやって作るのかを説明した資料";
+echo '<br>';
+echo '<br>';
+echo "Git" . "<br>";
+echo "分散型バージョン管理システム、ファイルのバージョン管理が行えるツール、";
+echo '<br>';
+echo '<br>';
+echo "マージツール" . "<br>";
+echo "プロジェクトのバージョン間での比較や変更をマージするツール";
+
+?>
+
